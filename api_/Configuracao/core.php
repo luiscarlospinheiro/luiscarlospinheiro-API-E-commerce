50dab7aa16becbd1d1c83dae8c003d22c5abbab5
luiscarlospinheiro <77640139+luiscarlospinheiro@users.noreply.github.com>
2021-06-13 21:42:23 -0300

    Create core.php

---
+++ api_/Configuracao/core.php
@@ -0,0 +1,18 @@
+
+<?php
+// mostra erros
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// url
+$home_url=""https://venanciov98.myvtex.com";
+
+// página fornecida no parâmetro de URL, a página padrão é um
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+
+
+$records_per_page = 5;
+
+
+$from_record_num = ($records_per_page * $page) - $records_per_page;
+?>
